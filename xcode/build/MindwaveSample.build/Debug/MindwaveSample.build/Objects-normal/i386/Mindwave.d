662511b93772d2b050e27b018c2e0bf0a260a922
Keith Butters <keith@barbariangroup.com>
2013-09-11 16:58:49 -0400

    initila commit

---
+++ xcode/build/MindwaveSample.build/Debug/MindwaveSample.build/Objects-normal/i386/Mindwave.d
@@ -0,0 +1,3 @@
+dependencies: \
+  /Users/keith/Documents/development/_barbarianprojects/MindwaveSample/xcode/../../../cinder_master/blocks/Mindwave/src/cinder/Mindwave.cpp \
+  /Users/keith/Documents/development/_barbarianprojects/MindwaveSample/xcode/../../../cinder_master/blocks/Mindwave/src/cinder/Mindwave.h
---
+++ xcode/build/MindwaveSample.build/Debug/MindwaveSample.build/Objects-normal/i386/MindwaveSampleApp.d
@@ -0,0 +1,4 @@
+dependencies: \
+  /Users/keith/Documents/development/_barbarianprojects/MindwaveSample/xcode/../src/MindwaveSampleApp.cpp \
+  ../../../cinder_master/include/cinder/app/AppNative.h \
+  /Users/keith/Documents/development/_barbarianprojects/MindwaveSample/xcode/../../../cinder_master/blocks/Mindwave/src/cinder/Mindwave.h
