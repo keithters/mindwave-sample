dbf993e95aa9272414849d0dbb76ea0796ae2286
Keith Butters <keith@barbariangroup.com>
2013-09-11 17:07:46 -0400

    removing build files from repo

--- xcode/build/MindwaveSample.build/Debug/MindwaveSample.build/Objects-normal/i386/MindwaveSampleApp.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/keith/Documents/development/_barbarianprojects/MindwaveSample/xcode/../src/MindwaveSampleApp.cpp \
-  ../../../cinder_master/include/cinder/app/AppNative.h \
-  /Users/keith/Documents/development/_barbarianprojects/MindwaveSample/xcode/../../../cinder_master/blocks/Mindwave/src/cinder/Mindwave.h
