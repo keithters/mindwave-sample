f2bf2f4682e724b492344f2dd5f2673b55b398ef
Keith Butters <keith@barbariangroup.com>
2013-09-11 17:01:39 -0400

    adding .gitignore

--- xcode/build/MindwaveSample.build/Debug/MindwaveSample.build/Objects-normal/i386/Mindwave.d
+++
@@ -1,3 +0,0 @@
-dependencies: \
-  /Users/keith/Documents/development/_barbarianprojects/MindwaveSample/xcode/../../../cinder_master/blocks/Mindwave/src/cinder/Mindwave.cpp \
-  /Users/keith/Documents/development/_barbarianprojects/MindwaveSample/xcode/../../../cinder_master/blocks/Mindwave/src/cinder/Mindwave.h
